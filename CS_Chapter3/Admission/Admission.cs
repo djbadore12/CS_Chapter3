6f380446564f71603a7ce4514f951ca3afd44be7
djbadore12 <djbadore12@yahoo.com>
2017-02-13 14:12:29 -0500

    Did a ton of em
    too lazy to type

---
+++ CS_Chapter3/Admission/Admission.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Admission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's Your GPA?");
+            double gpaOne = Convert.ToDouble( Console.ReadLine() );
+            Console.WriteLine("What's Your Test Score?");
+            double testScoreOne = Convert.ToDouble(Console.ReadLine());
+
+            if (gpaOne >= 3.0 && testScoreOne >= 60 || gpaOne <= 3.0 && testScoreOne >= 80)
+            {
+                Console.WriteLine("ACCEPT");
+            } 
+            else
+            {
+                Console.WriteLine("REJECT");
+            }
+
+        }
+    }
+}
---
+++ CS_Chapter3/Furniture/Furniture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Would you like Oak, Pine, or Mahogony?");
+            String wood = Console.ReadLine();
+            int pine = 100;
+            int oak = 225;
+            int mahogany = 310;
+
+            // if (wood == "P")
+            if (wood.Equals("P"))
+            {
+                Console.WriteLine("It will cost you $" + pine);
+            }
+            else if (wood.Equals("O"))
+            {
+                Console.WriteLine("It will cost you $" + oak);
+            }
+            else if (wood.Equals("M"))
+            {
+                Console.WriteLine("It will cost you $" + mahogany);
+            }
+            else
+            {
+                Console.WriteLine("$" + 0);
+            }
+
+        }
+    }
+}
